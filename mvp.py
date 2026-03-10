330029a0ae0d14ee903e2d5721392dbd26860b5b
mathildebodet <mathildebodet@orange.fr>
2026-03-10 17:15:11 +0100

    pie size

--- mvp.py
+++ mvp.py
@@ -1,88 +1,33 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-
-df = pd.read_csv("food_coded.csv")
-
-# st.title("What Shapes Student Eating Habits?")
-
-# constraint = st.selectbox(
-#     "Choose a constraint to explore:",
-#     ["Budget", "Time constraints", "Academic stress"]
-# )
-
-# # BUDGET
-# if constraint == "Budget":
-
-#     st.header("Income and diet quality")
-
-#     fig, ax = plt.subplots()
-#     df.groupby("income")[["fruit_day","veggies_day"]].mean().plot(kind="bar", ax=ax)
-#     ax.set_ylabel("Average portions per day")
-#     st.pyplot(fig)
-
-#     st.header("Income and eating out")
-
-#     fig, ax = plt.subplots()
-#     sns.countplot(data=df, x="income", hue="eating_out", ax=ax)
-#     ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
-#     st.pyplot(fig)
-
-
-# # TIME
-# elif constraint == "Time constraints":
-
-#     st.header("Cooking frequency")
-
-#     fig, ax = plt.subplots()
-#     df["cook"].value_counts().plot(kind="bar", ax=ax)
-#     ax.set_ylabel("Number of students")
-#     st.pyplot(fig)
-
-#     st.header("Cooking vs eating out")
-
-#     fig, ax = plt.subplots()
-#     sns.countplot(data=df, x="cook", hue="eating_out", ax=ax)
-#     ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
-#     st.pyplot(fig)
-
-
-# # STRESS
-# elif constraint == "Academic stress":
-
-#     st.header("Reasons for comfort food")
-
-#     fig, ax = plt.subplots()
-#     df["comfort_food_reasons"].value_counts().plot(kind="bar", ax=ax)
-#     ax.set_ylabel("Number of students")
-#     st.pyplot(fig)
-
-#     st.header("Eating changes since college")
-
-#     fig, ax = plt.subplots()
-#     df["eating_changes"].value_counts().plot(kind="bar", ax=ax)
-#     ax.set_ylabel("Number of students")
-#     st.pyplot(fig)
-
-import streamlit as st
 import plotly.express as px
 from streamlit_plotly_events import plotly_events
+
+df = pd.read_csv("food_coded.csv")
 
 labels = ["Budget", "Time", "Workload"]
 values = [40, 35, 25]
 col1, col2 = st.columns([1,2])
 
+st.title("What Shapes Student Eating Habits?")
+
 with col1:
     st.write("Click on a constraint to explore how it shapes student eating habits.")
 
+    pie_df = pd.DataFrame({
+    "constraint": labels,
+    "value": values
+    })
+
     fig = px.pie(
-        names=labels,
-        values=values,
+    pie_df,
+    names="constraint",
+    values="value"
     )
 
-    fig.update_traces(textinfo="label", textposition="inside", marker=dict(colors=["#FF9999", "#66B2FF", "#99FF99"]))
-    fig.update_layout(showlegend=False)
+    fig.update_traces(textinfo="label", textposition="inside", marker=dict(colors=["#FF9999", "#66B2FF", "#99FF99"]), hoverinfo = "skip", hovertemplate = None)
+    fig.update_layout(showlegend=False, width=400, height=400)
 
     selected = plotly_events(fig, click_event=True)
 
